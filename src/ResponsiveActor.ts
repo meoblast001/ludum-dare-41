4bfce8c432c300795a64fed8570e0d830971eeeb
nek0 <nek0@chelnok.de>
2018-04-21 22:10:46 +0200

    change astar AGAIN

--- src/ResponsiveActor.ts
+++ src/ResponsiveActor.ts
@@ -1,16 +1,15 @@
 import * as ex from 'excalibur';
-import { IAAStarField, IAAStarFieldNode, AAStar, AAStarSimpleField, SimpleNode }
-  from "aastar";
+import * as path from 'pathfinding';
 import { Executor, ExecutionActor } from './actionSystem/Executor';
 
 export class ResponsiveActor extends ex.Actor implements ExecutionActor
 {
   public readonly name: string;
   private _defaultSequence: string;
-  private astarInstance: AAStar;
-  private astarGrid: AAStarSimpleField;
+  private astarInstance: path.AStarFinder;
+  private astarGrid: path.Grid;
   private gridPos: [number, number];
-  private path: IAAStarFieldNode[];
+  private path: number[][];
 
   public constructor(
     name: string,
@@ -20,18 +19,20 @@ export class ResponsiveActor extends ex.Actor implements ExecutionActor
   {
     super(config);
     this.gridPos = [this.pos.x, this.pos.y];
-    this.path = new Array();
+    this.path = [];
     this.name = name;
     this._defaultSequence = defSeq;
-    this.astarGrid = new AAStarSimpleField(matrix);
-    this.astarInstance = new AAStar(this.astarGrid);
+    this.astarGrid = new path.Grid(matrix);
+    this.astarInstance = new path.AStarFinder({
+      diagonalMovement: path.DiagonalMovement.Never
+    });
   }
 
   public move(target: [number, number]) {
     // TODO: Perform move through A*.
-    this.path = this.astarInstance.calculatePath(
-      this.astarGrid.getNodeAt(this.gridPos[0], this.gridPos[1]),
-      this.astarGrid.getNodeAt(target[0], target[1]));
+    this.path = this.astarInstance.findPath(
+      this.gridPos[0], this.gridPos[1],
+      target[0], target[1], this.astarGrid);
   }
 
   public changeDefaultSequence(sequence: string) {
