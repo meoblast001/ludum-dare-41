8fad36244bf9f8f3f34cb1d5425739854271b478
nek0 <nek0@chelnok.de>
2018-04-22 11:02:01 +0200

    add tiles to GameplayScene

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -3,6 +3,7 @@ export interface Configuration {
   resources: Resources;
   player: Player;
   actors: Actor[];
+  map: MapDefintion;
 }
 
 export interface Resources {
@@ -27,4 +28,27 @@ export interface Textureable {
 
 export interface Spriteable {
   sprite?: string;
+}
+
+export interface MapDefintion {
+  cells: MapCellDefinition[];
+  tileSheets: MapTileSheet[];
+  width: number;
+  height: number;
+  tileWidth: number;
+  tileHeight: number;
+}
+
+export interface MapCellDefinition {
+  x: number;
+  y: number;
+  tileId: number;
+  sheetId: number;
+}
+
+export interface MapTileSheet {
+  id: number;
+  path: string;
+  cols: number;
+  rows: number;
 }
--- src/GameplayScene.ts
+++ src/GameplayScene.ts
@@ -12,9 +12,18 @@ export default class GameplayScene extends ex.Scene implements ExecutionWorld {
   public static readonly FriendActorName = "friend";
 
   private namedActors: { [name: string]: ResponsiveActor } = {};
+  private tileMap: ex.TileMap;
 
   public constructor(private config: Config.Configuration, engine?: ex.Engine) {
     super(engine)
+    this.tileMap = new ex.TileMap(
+      0,
+      0,
+      config.map.tileWidth,
+      config.map.tileHeight,
+      config.map.width / config.map.tileWidth,
+      config.map.height / config.map.tileHeight
+    );
   }
 
   public onInitialize(engine: ex.Engine) {
@@ -56,6 +65,24 @@ export default class GameplayScene extends ex.Scene implements ExecutionWorld {
     for (let actorConfig of config.actors) {
       this.addActor(actorConfig);
     }
+    // build sprite sheets
+    this.config.map.tileSheets.forEach(sheet => {
+      this.tileMap.registerSpriteSheet(
+        sheet.id.toString(),
+	new ex.SpriteSheet(
+	  new ex.Texture(sheet.path),
+	  sheet.cols,
+	  sheet.rows,
+	  this.config.map.tileWidth,
+	  this.config.map.tileHeight
+	)
+      );
+    });
+    // fill cells with sprites
+    this.config.map.cells.forEach(cell => {
+      let ts = new ex.TileSprite(cell.sheetId.toString(), cell.tileId);
+      this.tileMap.getCell(cell.x, cell.y).pushSprite(ts);
+    });
   }
 
   private addPlayer(config: Config.Player) {
